d4201cebdbd6c4a86a2b7f47951f89c1c970b4e3
scherepanov <sergey@cherepanov.org.ua>
2020-06-02 22:34:23 +0400

    replace pf with gobetween

--- Formula/docker-virtualbox.rb
+++ Formula/docker-virtualbox.rb
@@ -2,20 +2,37 @@ require 'formula'
 
 class DockerVirtualbox < Formula
   url "https://github.com/sergeycherepanov/homebrew-docker-virtualbox.git", :using => :git
-  version "0.0.2"
-  revision 2
+  version "0.0.3"
+  revision 5
 
+  depends_on 'curl'
+  depends_on 'jq'
   depends_on 'terminal-notifier'
   depends_on 'docker-cli'
   depends_on 'docker-compose'
   depends_on 'docker-credential-helper'
   depends_on 'docker-machine'
   depends_on 'docker-machine-nfs'
-  depends_on 'docker-machine-pf'
 
   keg_only "this package may conflict with official docker client"
 
+  resource "gobetween" do
+    url "https://github.com/yyyar/gobetween/releases/download/0.7.0/gobetween_0.7.0_darwin_amd64.zip"
+    sha256 "f679478b4ec13cce19309fdaee0a03da8940796dcf2a6a01dce2a8f9c0d9b9b0"
+  end
+
   def install
+    resource("gobetween").stage do
+      bin.install "gobetween"
+    end
+    (buildpath/"gobetween.toml").write <<~EOS
+      [api]
+      enabled = true
+      bind = "127.0.0.1:8181"
+    EOS
+    (etc/"docker-virtualbox").mkpath
+    etc.install "gobetween.toml" => 'docker-virtualbox/gobetween.toml'
+
     bin.install "bin/docker"
     bin.install "bin/docker-compose"
     bin.install "bin/docker-machine-init"
