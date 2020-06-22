95bffc6e81c5ce490b609d0eaf6e28a7417f411d
scherepanov <sergey@cherepanov.org.ua>
2020-06-23 03:25:15 +0400

    update revision

--- Formula/docker-virtualbox.rb
+++ Formula/docker-virtualbox.rb
@@ -3,7 +3,7 @@ require 'formula'
 class DockerVirtualbox < Formula
   url "https://github.com/sergeycherepanov/homebrew-docker-virtualbox.git", :using => :git
   version "0.0.3"
-  revision 10
+  revision 11
 
   depends_on 'curl'
   depends_on 'jq'
