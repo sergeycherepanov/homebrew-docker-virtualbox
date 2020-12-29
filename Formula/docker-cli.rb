e44fbd4ab0f2a2a8a47f8073704fe76e5bfff19b
Daniel Brooks <bass_rock@me.com>
2020-12-29 15:09:59 -0800

    updating docker cli

--- Formula/docker-cli.rb
+++ Formula/docker-cli.rb
@@ -3,14 +3,14 @@ require 'formula'
 class DockerCli < Formula
   homepage 'https://github.com/docker/cli'
   if OS.linux?
-    url 'https://download.docker.com/linux/static/stable/x86_64/docker-19.03.8.tgz'
-    sha256 '7f4115dc6a3c19c917f8b9664d7b51c904def1c984e082c4600097433323cf6f'
+    url 'https://download.docker.com/linux/static/stable/x86_64/docker-20.10.1.tgz'
+    sha256 '8790f3b94ee07ca69a9fdbd1310cbffc729af0a07e5bf9f34a79df1e13d2e50e'
   else
-    url 'https://download.docker.com/mac/static/stable/x86_64/docker-19.03.8.tgz'
-    sha256 'fe65c0039959b9ca326471427f26b1fcbd7b45079fe6e773c987c4fbbf87be71'
+    url 'https://download.docker.com/mac/static/stable/x86_64/docker-20.10.1.tgz'
+    sha256 'bfcbfa86b7df1c1312293ccf7faf29dd55ec501e5bbdc9cb38eb47ed976e554c'
   end
-  version "19.03.8"
-  revision 2
+  version "20.10.1"
+  revision 3
 
   keg_only "this package may conflict with official docker client"
 
