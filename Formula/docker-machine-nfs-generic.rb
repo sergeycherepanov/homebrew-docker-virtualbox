a7eb1aebe40df70987ff6c6c333c51f7135304da
Sergey Cherepanov <sergey@cherepanov.org.ua>
2021-03-31 02:59:34 +0400

    Update docker-machine-nfs-generic.rb

--- Formula/docker-machine-nfs-generic.rb
+++ Formula/docker-machine-nfs-generic.rb
@@ -1,11 +1,11 @@
 class DockerMachineNfsGeneric < Formula
   desc "Activates NFS on docker-machine (with generic driver support)"
   homepage "https://github.com/sergeycherepanov/docker-machine-nfs"
-  url "https://github.com/sergeycherepanov/docker-machine-nfs/archive/0.5.4.1.tar.gz"
-  sha256 "cec306b86a9b1d3e52768a813a40ab833a1f95becbf6e07b1aea6c4efc60c611"
+  url "https://github.com/sergeycherepanov/docker-machine-nfs/archive/0.5.4.2.tar.gz"
+  sha256 "5067da89d8ba1205883cfa0087106cbbe5bc4140ee4b9f45ed1a7b772363417b"
   license "MIT"
   version "0.5.4"
-  revision 1
+  revision 2
 
   bottle :unneeded
 
